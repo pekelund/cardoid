3af0e80dfb4340d4780a14ad0818a828866f21dc
Pär Ekelund <pekelund@gmail.com>
2020-06-18 10:48:18 +0200

    Update cardoid.js

--- cardoid.js
+++ cardoid.js
@@ -38,11 +38,17 @@ function getY(y) {
 	return center_y + radius * Math.sin(-angle*Math.PI/180);	
 }
 
+function drawCircle() {
+	ctx.fillStyle = 'rgb(0, 0, 255)';
+	ctx.beginPath();
+	ctx.arc(300, 300, 250, degToRad(0), degToRad(360), false);
+	ctx.stroke();
+}
+
 function drawPoint(angle,distance,label){
 	var angle = 360/num_points*angle;
-  var x = center_x + radius * Math.cos(-angle*Math.PI/180) * distance;
+  	var x = center_x + radius * Math.cos(-angle*Math.PI/180) * distance;
 	var y = center_y + radius * Math.sin(-angle*Math.PI/180) * distance;
-  //console.log("drawPoint: ", angle, distance, label, x, y);
 
 	ctx.beginPath();
 	ctx.arc(x, y, point_size, 0, 2 * Math.PI);
@@ -54,7 +60,6 @@ function drawPoint(angle,distance,label){
 
 function addDots(ctx) {
   for (i = 0; i < num_points; i++) {
-	  //console.log("Drawing dot ", i, i*36);
 	  drawPoint(i, 1, i);
 	}
 }
@@ -66,18 +71,13 @@ function connectPoints(point1, point2) {
 	x2 = getX(toPoint);
 	y2 = getY(toPoint);
 
-	//console.log("connectPoints: ", point1, point2, x1, y1, x2, y2);
 	ctx.fillStyle = 'rgb(0, 0, 255)';
 	ctx.lineWidth = 1;
 
-  ctx.beginPath();
+  	ctx.beginPath();
 	ctx.moveTo(x1, y1);
 	ctx.lineTo(x2, y2);
 	ctx.stroke();
-	//ctx.beginPath();
-  //ctx.arc(x1, y1, point_size*2, 0, 2 * Math.PI);
-  //ctx.arc(x2, y2, point_size*2, 0, 2 * Math.PI);
-  //ctx.fill();
 }
 
 function sleep(ms) {
@@ -90,22 +90,18 @@ function updateValues() {
 
 async function demo() {
 	getValues();
-	ctx.fillStyle = 'rgb(0, 0, 255)';
-	ctx.beginPath();
-	ctx.arc(300, 300, 250, degToRad(0), degToRad(360), false);
-	ctx.stroke();
 
+	drawCircle();
 	addDots(ctx);
 
 	for (m=0; m < iterations; m=m+increment) {
-		console.log("Mult: ", m + mult);
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		drawCircle();
 		for (dot = 0; dot < num_points; dot++) {
 			//console.log("connect points: ", dot, dot*mult);
 			connectPoints(dot, dot*(m+mult));
 			await sleep(delay_line);
 		}
 		await sleep(delay_iteration);
-
 	}
 }
